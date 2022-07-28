296dc9eff76c27760b252d11e798cb2c97ac58bf
avif muhamad taufiq <avifmuhamadtaufiq4@gmail.com>
2022-07-28 12:14:37 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,18 +12,17 @@ bluetooth.onBluetoothDisconnected(function () {
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
-        kompor1_level += 10
+        kompor1_level += Math.map(10, 0, 100, 0, 1023)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
-        kompor2_level += 10
+        kompor1_level += Math.map(10, 0, 100, 0, 1023)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
-        kompor1_level += 0
+        kompor1_level += Math.map(10, 0, 100, 1023, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
-        kompor2_level += 0
+        kompor2_level += Math.map(10, 0, 100, 1023, 0)
     } else {
     	
     }
 })
-let selected_kompor = ""
 let kompor1_level = 0
 let kompor2_level = 0
 basic.forever(function () {
