fb2bfb48d7243163c789765416f2633bb78ce317
avif muhamad taufiq <avifmuhamadtaufiq4@gmail.com>
2022-07-28 12:09:02 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . . . # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . . . # .
+        `)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        kompor1_level += 10
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        kompor2_level += 10
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+        kompor1_level += 0
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        kompor2_level += 0
+    } else {
+    	
+    }
+})
+let selected_kompor = ""
+let kompor1_level = 0
+let kompor2_level = 0
 basic.forever(function () {
-	
+    pins.analogWritePin(AnalogPin.P1, kompor1_level)
+    pins.analogWritePin(AnalogPin.P2, kompor2_level)
 })
